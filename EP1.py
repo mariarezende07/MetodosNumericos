610967fd23f5df89c91c9cb8fcf01920631065f9
mariarezende07 <maloasan@gmail.com>
2022-04-28 08:30:50 -0300

    ✨ Add resolução matriz ciclica

--- EP1.py
+++ EP1.py
@@ -5,41 +5,55 @@ import time
 def tridigonal_ciclic_matrix(n):
     index = range(1,n+1)
     a = list(map((lambda x : (2*x -1)/(4*x) if x <= n - 1 else (2*n -1)/(2*n)), index))
-    b = [2] * (n+1)
+    b = [2] * (n)
     c = list(map((lambda x: 1- a[x-1]), index))
     d= list(map((lambda x: np.cos((2 * math.pi * x**2)/n**2)),index))
 
-    return([a,b,c,d])
+    A = np.array([a,b,c])
+    return(A,d)
 
-def decompose_LU(vetores, n):
-    u = np.zeros(n)
-    l = np.zeros(n)
-    u[0] = vetores[2][0]
+def decompose_LU(A, n):
+    u = np.empty(n)
+    l = np.empty(n)
+    u[0] = A[2][0]
     for i in range(1,n):
-        l[i] = (vetores[0][i]/u[i-1])
-        u[i] = vetores[1][i] - ( l[i] * vetores[2][i-1] )
+        l[i] = (A[0][i]/u[i-1])
+        u[i] = A[1][i] - ( l[i] * A[2][i-1] )
     return([l,u])
 
 
-def tridigonal_system(LU,vetores,n):
+def tridigonal_system(LU,A,d,n):
     y = np.empty(n)
-    y[0] = vetores[3][0]
-    y[1:] = [vetores[3][i] - LU[1][i]*y[i-1] for i in range(1,n)]
+    y[0] = d[0]
+    y[1:] = [d[i] - LU[1][i]*y[i-1] for i in range(1,n)]
     
     x = np.empty(n)
     x[n-1] = y[n-1]/LU[1][n-1]
-    x[0:n-2] = [(y[i]-(vetores[2][i]*x[i+1]))/LU[1][i] for i in range(1,n-1)]
-    return [x,y]
+    x[0:n-2] = [(y[i]-(A[2][i]*x[i+1]))/LU[1][i] for i in range(1,n-1)]
+    return x
 
-# def tridigonal_ciclic_system(LU,vetores,n):
-    
-#     x = np.empty(n)
-#     x[n-1] = (vetores[3][n-1] - (vetores[2][n-1]*))()
-#     return
+def tridigonal_ciclic_system(LU,A,d,n):
+    v = np.zeros(n)
+    w = np.zeros(n)
+
+    v[0] = A[0][0]
+    v[n-1] = A[2][n-2]
+
+    w[0] = A[2][n-1]
+    w[n-1] = A[0][n-1]
+
+    z_til = tridigonal_system(LU, [0,0,A[2]], v, n-1)
+    y_til = tridigonal_system(LU, [0,0,A[2]], d, n-1)
+
+    x = np.empty(n)
+    x[n-1] = (d[n-1] - (A[2][n-1]* y_til[0]) - (A[0][n-1]*y_til[n-2]))/(A[1][n-1] - (A[2][n-2]*z_til[0]) - (A[0][n-1]*z_til[n-2])) 
+    x[0:n-2] = [y_til[i] - (x[n-1]*z_til[i]) for i in range(1,n-1)]
+
+    return x
 tic = time.perf_counter()
-vetores = tridigonal_ciclic_matrix(20)
-LU = decompose_LU(vetores, 20)
-tridigonal_system(LU, vetores, 20)
+A,d = tridigonal_ciclic_matrix(20)
+LU = decompose_LU(A, 20)
+tridigonal_ciclic_system(LU, A,d, 20)
 toc = time.perf_counter()
 print("meu:",toc-tic)
 # 
