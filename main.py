9f08d971727d2b334e29faf5f6aa6c8b461511b0
mariarezende07 <maloasan@gmail.com>
2022-05-01 18:53:15 -0300

    ✨ Add main e script de testes

---
+++ main.py
@@ -0,0 +1,29 @@
+from tests import *
+
+def main():
+    """Função principal da aplicação.
+    """
+    modo = input("1- Matriz por dimensão\n2- Verificar tempo do algoritmo\nDefina o modo: ")
+
+    if modo == "1":
+        n = int(input("Digite a dimensão da matriz: "))
+        A, d, x = matrix_from_dimension(n)
+
+        x_str = ["x_"+ str(i) for i in range(1,n+1)]
+        print(f"A matriz A consiste em:\na = {A[0]}\nb={A[1]}\nc={A[2]}\n")
+        print(f"O vetor d consiste em:\nd={d}\n")
+
+        solucoes = np.array([ x_str[i] + " = " + str(x[i]) for i in range(n)]).reshape(n,1)
+        
+        print(f"As soluções são:{solucoes}")
+    
+    if modo =="2":
+        inicio = int(input("Insira o início do intervalo: "))
+        fim = int(input("Insira o fim do intervalo: "))
+        step = int(input("Insira o step do intervalo: "))
+
+        time_taken(inicio,fim,step)
+
+if __name__ == "__main__":
+    main()
+
---
+++ tests.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from numpy import ndarray
+from functions import *
+
+def matrix_from_dimension(n:int)-> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Calcula um sistema tridiagonal cíclico a partir da dimensão dele
+
+    Args:
+        n (int): Dimensão da matriz do sistema
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray]: Retorna 3 arrays, sendo elas a matriz do sistema, o lado
+        direito do sistema e as soluções
+    """
+    A,d = tridigonal_ciclic_matrix(n)
+    x = tridigonal_ciclic_system(A,d, n)
+    return A, d, x
+
+def time_taken(inicio:int,fim:int,step:int) -> None:
+    """Calcula o tempo tomado para calcular a solução de um sistema tridiagonal cíclico dado um intervalo
+    e plota um gráfico
+
+    Args:
+        inicio (int): Início do intervalo
+        fim (int): Fim do intervalo
+        step (int): Steps do intervalo
+    """
+    x = list(range(inicio,fim,step))
+    tempos = []
+    for n in range(inicio,fim,step):
+        start = timer()
+        matrix_from_dimension(n)
+        end = timer()
+        tempos.append(end-start)
+    plt.plot(x,tempos)
+    plt.show()
+
