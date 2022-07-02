e5408789ea6b4065c6e98884e615b3ea8aaddf11
mariarezende07 <maloasan@gmail.com>
2022-07-02 19:53:33 -0300

    ✨ Add integral unica

--- src/integrals.py
+++ src/integrals.py
@@ -1,6 +1,7 @@
 from typing import List, Tuple
 import pandas as pd
 import numpy as np
+from preprocessing_data import *
 
 def gauss_formula(intervalo:List,funcao:str,no:pd.DataFrame) -> float:
     """Aplica a formula de gauss para resolver uma integral dupla dado um determinado intervalo
@@ -42,3 +43,35 @@ def gauss_formula(intervalo:List,funcao:str,no:pd.DataFrame) -> float:
     # Multiplica a integral I por uma constante para ajustar o valor no intervalo dado
     I *= (b-a)/2
     return I
+
+def gauss_formula_simple(intervalo:List,funcao:str,no:pd.DataFrame) -> float:
+    """Aplica a formula de gauss para resolver uma integral simples dado um determinado intervalo
+
+    Args:
+        intervalo (List[int,int]): Lista com os valores dos intervalos a,b
+        funcao (str): String representando a funcao
+        no (pd.DataFrame): Tabela dos nós e seus respectivos pesos
+
+    Returns:
+        float: Valor numérico da integração
+    """
+    # Determina o tamanho da lista de nós que estamos trabalhando e os intervalos
+    n = len(no)    
+    a = intervalo[0]
+    b = intervalo[1]
+
+    F = 0
+    # Realiza as duas somatórias necessárias
+    for i in range(n):
+        x_i = no["x_j"][i]
+        w_i = no["w_j"][i]
+        # Ajusta X e Y no intervalo dado
+        x = ((b-a)*x_i + a + b)/2     
+        F += eval(funcao) * w_i  # Dada a função f(x,y), calcula os valores com os resultados obtidos e pelo peso
+        
+    # Multiplica a integral I por uma constante para ajustar o valor no intervalo dado
+    F *= (b-a)/2
+    return F
+
+
+print(gauss_formula_simple([1,2],"1/x",n6))
